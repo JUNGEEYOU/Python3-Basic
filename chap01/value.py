db120e673d85fad7160b1ab8bb206445b37db0f3
JUNGEEYOU <junge2u@naver.com>
2017-11-03 20:54:00 +0900

    변수에 대한 예제

---
+++ chap01/value.py
@@ -0,0 +1,17 @@
+# 변수: 데이터를 담는 메모리 공간
+
+val1 = 2000
+print("val1  값 : ", val1 )
+print("val1 의 type: ", type(val1 ))
+
+
+val2 = 20.3
+print("val2  값 : ", val2 )
+print("val2 의 type: ", type(val2))
+
+
+val3 = "hello Python"
+print("val3  값 : ", val3 )
+print("val3 의 type: ", type(val3))
+
+
