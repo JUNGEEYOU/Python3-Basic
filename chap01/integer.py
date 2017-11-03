3d46705ceef6a19a892e4061b50d3a834472f94f
JUNGEEYOU <junge2u@naver.com>
2017-11-03 21:18:52 +0900

    수 다루기 - 정수

---
+++ chap01/integer.py
@@ -0,0 +1,42 @@
+
+# 01. 정수
+#1) 연산: 덧셈 + , 뺄셈 - , 곱셈 * , 나눗셈 /, 몫 //, 나머지 %
+val1 = 3 + 4 
+print("1. 덧셈 결과 val1: ",val1)
+
+val2 = 3 - 4
+print("2. 뺄셈 결과 val2: ",val2)
+
+val3 = 3 * 4 
+print("3. 곱셈 결과 val3: ",val3)
+
+
+val4 = 3 / 4 
+print("4. 나눗셈 결과 val4: ",val4)
+
+val5 = 9 // 4 
+print("5. 나눗셈  몫 결과 val5: ",val5)
+
+
+val6 = 9 % 4 
+print("6. 나눗셈 나머지 결과 val6: ",val6)
+
+
+# 2) 여러 가지 진법으로 수 출력하기 
+# - 16진수 
+print("hex(0) 값: ",hex(0))
+print("hex(255) 값: ",hex(255))
+val7 = 0xff
+print("val7 값: ", val7)
+
+# - 2 진수 
+print("bin(0) 값: ",bin(0))
+print("bin(255) 값: ",bin(255))
+val8 = 0b100
+print("val8 값: ", val8)
+
+# - 8진수 
+print("oct(8) 값: ",oct(8))
+print("oct(10) 값: ",oct(10))
+val9 = 0o10
+print("val9 값: ", val9)
